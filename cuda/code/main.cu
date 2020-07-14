a4179864b07404ea857fc7e94d86b90556df8ae2
Guo-Haowei <ghwhotline@gmail.com>
2020-07-14 10:05:58 -0700

    Cuda output image

---
+++ cuda/code/main.cu
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <time.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+// limited version of checkCudaErrors from helper_cuda.h in CUDA examples
+#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
+    if (result) {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+            file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting
+        cudaDeviceReset();
+        exit(99);
+    }
+}
+
+__global__ void render(float *fb, int max_x, int max_y) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j*max_x*3 + i*3;
+    fb[pixel_index + 0] = float(i) / max_x;
+    fb[pixel_index + 1] = float(j) / max_y;
+    fb[pixel_index + 2] = 0.0;
+}
+
+int main() {
+    int nx = 1200;
+    int ny = 600;
+    int tx = 8;
+    int ty = 8;
+
+    std::cerr << "Rendering a " << nx << "x" << ny << " image ";
+    std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+
+    int num_pixels = nx*ny;
+    size_t fb_size = 3*num_pixels*sizeof(float);
+
+    // allocate FB
+    float *fb;
+    checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
+
+    clock_t start, stop;
+    start = clock();
+    // Render our buffer
+    dim3 blocks(nx/tx+1,ny/ty+1);
+    dim3 threads(tx,ty);
+    render<<<blocks, threads>>>(fb, nx, ny);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+    stop = clock();
+    double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+    std::cerr << "took " << timer_seconds << " seconds.\n";
+
+    // Output FB as Image
+    unsigned char* imageBuffer = new unsigned char[3 * nx * ny];
+    int ibIdx = 0;
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            size_t pixel_index = j*3*nx + i*3;
+            float r = fb[pixel_index + 0];
+            float g = fb[pixel_index + 1];
+            float b = fb[pixel_index + 2];
+            imageBuffer[ibIdx++] = unsigned char(255.99f * r);
+            imageBuffer[ibIdx++] = unsigned char(255.99f * g);
+            imageBuffer[ibIdx++] = unsigned char(255.99f * b);
+        }
+    }
+
+    checkCudaErrors(cudaFree(fb));
+
+    // write to png
+    stbi_write_png("test.png", nx, ny, 3, imageBuffer, 3 * nx);
+    delete imageBuffer;
+}
